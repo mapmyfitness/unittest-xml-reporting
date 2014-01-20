72aab05ea30a7e87db1f098a9c177d0f7b7d4dae
Dave McLain <dave.mclain@mapmyfitnessinc.com>
2014-01-20 15:16:03 -0600

    CORE-472 prep for internal relase

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import sys
 from setuptools import setup, find_packages
 
 # Load version information
@@ -11,9 +12,18 @@ xmlrunner_version = os.path.join(
 exec(compile(open(xmlrunner_version).read(), xmlrunner_version, 'exec'))
 
 
+if 'sdist' in sys.argv:
+    import mmf_release_tools
+    version = mmf_release_tools.generate_release_version(__version__, __file__)
+    mmf_release_tools.write_release_version(version)
+else:
+    with open("RELEASE-VERSION", "r") as f:
+        version = f.readlines()[0].strip()
+
+
 setup(
     name = 'unittest-xml-reporting',
-    version = __version__,
+    version = version,
     author = 'Daniel Fernandes Martins',
     author_email = 'daniel.tritone@gmail.com',
     description = 'PyUnit-based test runner with JUnit like XML reporting.',
